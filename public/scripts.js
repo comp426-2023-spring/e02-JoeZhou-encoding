72c8018b29b187e7ab5e53baf9877a7ea0b0ee43
JoeZhou-encoding <zeqi@live.unc.edu>
2023-05-09 18:23:31 -0400

    guess

--- public/scripts.js
+++ public/scripts.js
@@ -7,7 +7,82 @@
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
 
-function play(){
+function rpsN() {
+    const url = "/app/rps"
+    return response = fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            return data;
+        })
+        .catch(error => console.error(error));
+}
+
+function rpslsN() {
+    const url = "/app/rpsls"
+    return response = fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            return data;
+        })
+        .catch(error => console.error(error));
+}
+
+function rpsO(shot) {
+    const url = "/app/rps/play/" + shot
+    return response = fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            return data;
+        })
+        .catch(error => console.error(error));
+}
+
+function rpslsO(shot) {
+    const url = "/app/rpsls/play/" + shot
+    return response = fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            return data;
+        })
+        .catch(error => console.error(error));
+}
+
+
+
+
+function rps(shot){
+    document.getElementById("player-shot").value = shot;
+
+    rpsO(shot).then(someVal => {
+        result = someVal["result"];
+        computerShot = someVal["opponent"];
+        document.getElementById("result").value = result;
+        document.getElementById("opponent-shot").value = computerShot;
+    });
+}
+
+function rpsls(shot){
+    document.getElementById("rpsls-player-shot").value = shot;
+
+    rpslsO(shot).then(someVal => {
+        result = someVal["result"];
+        computerShot = someVal["opponent"];
+        document.getElementById("rpsls-result").value = result;
+        document.getElementById("rpsls-opponent-shot").value = computerShot;
+    });    
+}
+
+
+
+function Rules(){
+    window.location.href = "rules.html";
+}
+
+function back(){
+    window.location.href = "index.html";
+}
+
+function start(){
     var rps = document.getElementById('rps');
     var rpsls = document.getElementById('rpsls');
     var opponent = document.getElementById('opponent');
@@ -21,84 +96,14 @@ function play(){
     }
 
     if (!opponent.checked && rps.checked){
-        rpsNoOpponent().then(shot => {
+        rpsN().then(shot => {
             document.getElementById("no-opponent").value = shot["player"];
         })
     }
 
     if (!opponent.checked && rpsls.checked){
-        rpslsNoOpponent().then(shot => {
+        rpslsN().then(shot => {
             document.getElementById("no-opponent").value = shot["player"];
         })
     }
-}
-
-function rps(shot){
-    document.getElementById("player-shot").value = shot;
-
-    rpsOpponent(shot).then(someVal => {
-        result = someVal["result"];
-        computerShot = someVal["opponent"];
-        document.getElementById("result").value = result;
-        document.getElementById("opponent-shot").value = computerShot;
-    });
-}
-
-function rpsls(shot){
-    document.getElementById("rpsls-player-shot").value = shot;
-
-    rpslsOpponent(shot).then(someVal => {
-        result = someVal.result;
-        computerShot = someVal.opponent;
-        document.getElementById("rpsls-result").value = result;
-        document.getElementById("rpsls-opponent-shot").value = computerShot;
-    });    
-}
-
-function rpsNoOpponent() {
-    const url = "/app/rps"
-    return response = fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            return data;
-        })
-        .catch(error => console.error(error));
-}
-
-function rpslsNoOpponent() {
-    const url = "/app/rpsls"
-    return response = fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            return data;
-        })
-        .catch(error => console.error(error));
-}
-
-function rpsOpponent(shot) {
-    const url = "/app/rps/play/" + shot
-    return response = fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            return data;
-        })
-        .catch(error => console.error(error));
-}
-
-function rpslsOpponent(shot) {
-    const url = "/app/rpsls/play/" + shot
-    return response = fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            return data;
-        })
-        .catch(error => console.error(error));
-}
-
-function viewRules(){
-    window.location.href = "rules.html";
-}
-
-function home(){
-    window.location.href = "index.html";
 }
